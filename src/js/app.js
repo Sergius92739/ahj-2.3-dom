0a6dbb7ccaf7e281381041b5b6407313d1d01585
Sergius92739 <sergem279@gmail.com>
2021-08-24 13:48:41 +0300

    added README

---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+import data from './data';
+import Table from './Table';
+
+const table = new Table(data);
+
+table.bindToDOM(document.getElementById('container'));
+table.drawTable();
+table.startSorting();
